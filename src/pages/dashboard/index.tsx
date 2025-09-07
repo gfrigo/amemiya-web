07b6a2bae19c6069bb77db173c35d5cec65b85ea
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-09-07 16:19:54 -0300

    feat(react): adding react arch

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../../components/Button"
+
+export function Dashboard(){
+  return(
+    <div>
+        <h1 className="font-inter font-normal text-[20px] text-amemiya-black">
+          Dashboard
+        </h1>
+        <Button/>
+    </div>
+  )
+}
